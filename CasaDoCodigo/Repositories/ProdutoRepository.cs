0815fb507f15b12ac733ac15508f0ad2da1b8024
Jeihcio Francis <jeihcio.francis@hotmail.com>
2021-09-16 16:52:05 -0300

    Exibindo o carrosel com dados dinâmicos vindo do banco de dados

--- CasaDoCodigo/Repositories/ProdutoRepository.cs
+++ CasaDoCodigo/Repositories/ProdutoRepository.cs
@@ -15,6 +15,11 @@ namespace CasaDoCodigo.Repositories
             this.contexto = contexto;
         }
 
+        public IList<Produto> GetProdutos()
+        {
+            return contexto.Set<Produto>().ToList();
+        }
+
         public void SaveProdutos(List<Livro> livros)
         {
             foreach (var livro in livros)
@@ -24,6 +29,7 @@ namespace CasaDoCodigo.Repositories
 
             contexto.SaveChanges();
         }
+
     }
 
     public class Livro
