c3cc3e3195290aa4ae52214495ad7471b72705c6
Jeihcio Francis <jeihcio.francis@hotmail.com>
2021-09-17 12:10:50 -0300

    Adicionando uma sessão

--- CasaDoCodigo/Repositories/PedidoRepository.cs
+++ CasaDoCodigo/Repositories/PedidoRepository.cs
@@ -1,4 +1,5 @@
 ﻿using CasaDoCodigo.Models;
+using Microsoft.AspNetCore.Http;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -13,8 +14,20 @@ namespace CasaDoCodigo.Repositories
 
     public class PedidoRepository : BaseRepository<Pedido>, IPedidoRepository
     {
+        private readonly IHttpContextAccessor contextAccessor;
+
         public PedidoRepository(ApplicationContext contexto) : base(contexto)
         {
+        }
+
+        private int? GetPedidoId()
+        {
+            return contextAccessor.HttpContext.Session.GetInt32("pedidoId");
+        }
+
+        private void SetPedidoId(int pedidoId)
+        {
+            contextAccessor.HttpContext.Session.SetInt32("pedidoId", pedidoId);
         }
     }
 }
