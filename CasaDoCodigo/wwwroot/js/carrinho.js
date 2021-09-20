bc588f83bfcbeda2bc428527e1d33003929a0299
Jeihcio Francis <jeihcio.francis@hotmail.com>
2021-09-20 17:04:41 -0300

    Criando os métodos do front para atualizar a quantidade no carrinho

---
+++ CasaDoCodigo/wwwroot/js/carrinho.js
@@ -0,0 +1,42 @@
+﻿class Carrinho {
+    clickIncremento(btn) {
+        let data = this.getData(btn);
+        data.Quantidade++;
+        this.postQuantidade(data);
+    }
+
+    clickDecremento(btn) {
+        let data = this.getData(btn);
+        data.Quantidade--;
+        this.postQuantidade(data);
+    }
+
+    updateQuantidade(input) {
+        let data = this.getData(input);
+        this.postQuantidade(data);
+    }
+
+    getData(elemento) {
+        let linhaDoItem = $(elemento).parents('[item-id]');
+        let itemId = $(linhaDoItem).attr('item-id');
+        let novaQtde = $(linhaDoItem).find('input').val();
+
+        let data = {
+            Id: itemId,
+            Quantidade: novaQtde
+        };
+
+        return data;
+    }
+
+    postQuantidade(data) {
+        $.ajax({
+            url: '/pedido/updatequantidade',
+            type: 'POST',
+            contentType: 'application/json',
+            data: JSON.stringify(data)
+        });
+    }
+}
+
+var carrinho = new Carrinho();
