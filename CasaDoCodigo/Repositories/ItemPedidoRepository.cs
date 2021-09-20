3bbfbfd07871184040b8b3f444d8ef3c3c72499f
Jeihcio Francis <jeihcio.francis@hotmail.com>
2021-09-20 15:04:35 -0300

    Iniciando a implementação do método para atualizar a quantidade no banco de dados, usando o repository

--- CasaDoCodigo/Repositories/ItemPedidoRepository.cs
+++ CasaDoCodigo/Repositories/ItemPedidoRepository.cs
@@ -3,12 +3,16 @@ namespace CasaDoCodigo.Repositories
 {
     public interface IItemPedidoRepository
     {
-
+        void UpdateQuantidade(ItemPedido itemPedido);
     }
 
     public class ItemPedidoRepository : BaseRepository<ItemPedido>, IItemPedidoRepository
     {
         public ItemPedidoRepository(ApplicationContext contexto) : base(contexto)
+        {
+        }
+
+        public void UpdateQuantidade(ItemPedido itemPedido)
         {
 
         }
