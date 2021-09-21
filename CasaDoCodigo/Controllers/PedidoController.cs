5f4b5843561ddfac250fc20349c2fc61dd12bffa
Jeihcio Francis <jeihcio.francis@hotmail.com>
2021-09-21 16:08:44 -0300

    Atualizando o cadastro

--- CasaDoCodigo/Controllers/PedidoController.cs
+++ CasaDoCodigo/Controllers/PedidoController.cs
@@ -56,8 +56,12 @@ namespace CasaDoCodigo.Controllers
         [HttpPost]
         public IActionResult Resumo(Cadastro cadastro)
         {
-            Pedido pedido = pedidoRepository.GetPedido();
-            return View(pedido);
+            if (ModelState.IsValid)
+            {
+                return View(pedidoRepository.UpdateCadastro(cadastro));
+            }
+
+            return RedirectToAction("Cadastro");
         }
 
         [HttpPost]
